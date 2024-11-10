fc37c29bc2dab072e15c468dedb5fa88a9529d74
Rajat-pixel-codes <65059677+Rajat-pixel-codes@users.noreply.github.com>
2024-11-11 01:27:01 +0530

    implement the complete layout

---
+++ app/(auth)/sign-out/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const SignOut = () => {
+  return <div>SignOut</div>;
+};
+
+export default SignOut;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const TransactionHistory = () => {
+  return <div>TransactionHistory</div>;
+};
+
+export default TransactionHistory;
